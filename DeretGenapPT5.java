939fb628d03cebb6b3f49ff3b06c6d0ecdf1a852
Aobing99 <dodoco.awal@gmail.com>
2026-04-21 20:28:11 +0700

    Add files via upload

---
+++ DeretGenapPT5.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan3;
+
+import java.util.*;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class DeretGenapPT5 {
+        public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int a,total=0;
+        
+        for (a = 2; a <= 20; a+=2) {
+            System.out.print(a + "+");
+            total = total+a;
+        }
+        System.out.print(" = "+total);
+    }
+    
+}
